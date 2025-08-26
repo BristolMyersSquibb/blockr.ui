52923586e643fdf7ab10657df4eed52acf1ee1a9
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-08-26 15:09:18 +0200

    hotfix additional ui deps injection

--- R/dag-board.R
+++ R/dag-board.R
@@ -75,12 +75,17 @@ serve.dag_board <- function(x, id = "main", ...) {
     notify_user()
   )
 
-  ui <- page_fillable(
-    padding = 0,
-    gap = 0,
-    shinyjs::useShinyjs(),
-    add_busy_load_deps(main_ui(id, x, plugins)),
-    ...
+  ui <- do.call(
+    page_fillable,
+    c(
+      list(
+        padding = 0,
+        gap = 0,
+        shinyjs::useShinyjs(),
+        add_busy_load_deps(main_ui(id, x, plugins))
+      ),
+      unname(list(...))
+    )
   )
 
   server <- function(input, output, session) {
